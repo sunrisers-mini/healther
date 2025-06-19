05e6651272028369a9cbfb7fa757632a1a05231c
sunrisers-mini <minifinalpro@gmail.com>
2025-06-19 17:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ try:
     }
     project_id = st.secrets["WATSONX_PROJECT_ID"]
     llm = WatsonxLLM(
-        model_id="ibm/granite-3-2-8b-instruct",
+        model_id="ibm/granite-13b-code-instruct",
         url=credentials.get("url"),
         apikey=credentials.get("apikey"),
         project_id=project_id,
